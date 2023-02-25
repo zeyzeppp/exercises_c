5eac7a81df66a79b68283c231b4a916324cafc6e
zeyzeppp <123326315+zeyzeppp@users.noreply.github.com>
2023-02-25 16:11:28 +0300

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fibonacci(int);
+int main85()
+{
+    int x;
+    printf("Please enter a number: ");
+    scanf("%d", &x);
+    printf("%d", fibonacci(x));
+
+}
+int fibonacci(int x)
+{
+    if(x == 1 || x == 2)
+    {
+        return 1;
+    }
+    return (fibonacci(x - 1) + fibonacci(x - 2));
+}
