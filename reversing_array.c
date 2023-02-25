f681a660ce40719922f2a98156e39ece1f9e0abe
zeyzeppp <123326315+zeyzeppp@users.noreply.github.com>
2023-02-25 21:38:06 +0300

    Add files via upload

---
+++ reversing_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main82()
+{
+    int a[5];
+    int i;
+    int x;
+    for(i = 0; i < 5; i++)
+    {
+        printf("Please enter a number: ");
+        scanf("%d", &x);
+        a[i] = x;
+    }
+    for(i = 0; i < 5; i++)
+    {
+        printf("%d", a[4 - i]);
+    }
+}
