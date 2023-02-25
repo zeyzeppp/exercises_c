0d466979739a0ecf2060e5e4cb442089671a528e
zeyzeppp <123326315+zeyzeppp@users.noreply.github.com>
2023-02-25 20:47:31 +0300

    Add files via upload

---
+++ fibo_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int fibo(int);
+
+int main83()
+{
+    int y;
+    printf("Please enter a number: ");
+    scanf("%d", &y);
+    int array[y];
+    int i;
+    for(i = 0; i < y; i++)
+    {
+        array[i] = fibo(i + 1);
+        printf("%d ", array[i]);
+    }
+    return 0;
+}
+
+int fibo(int x)
+{
+    if(x == 1 || x == 2)
+    {
+        return 1;
+    }
+    return (fibo(x - 1) + fibo(x - 2));
+}
+
