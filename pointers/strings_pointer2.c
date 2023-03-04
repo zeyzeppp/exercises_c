23b2fc5786e6a8f40ebf0e034e9394aa293e5072
zeyzeppp <123326315+zeyzeppp@users.noreply.github.com>
2023-03-04 18:48:22 +0300

    Add files via upload

---
+++ pointers/strings_pointer2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char *fb(char *p, int index)
+{
+    int length = strlen(p);
+    if(index > length)
+    {
+        return NULL;
+    }
+    else
+    {
+        return p + index;
+    }
+}
+
+int main73()
+{
+    char name[] = "robin_one_persie";
+    printf("%s", fb(name, 0));
+
+    return 0;
+}
