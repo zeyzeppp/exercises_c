1111fc0bca35f2bfd90b4d608f2ff43b4eee42b1
zeyzeppp <123326315+zeyzeppp@users.noreply.github.com>
2023-03-21 11:20:55 +0300

    Add files via upload

---
+++ diff_condStatement.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main65()
+{
+    int not;
+    printf("lutfen notunuzu giriniz: ");
+    scanf("%d", &not);
+    printf("%s",not >= 50 ? "gectiniz" : "kaldiniz") ;
+
+}
