52d96c4b31a5ff2b6ffebb60092b890cc6aa27e2
zeyzeppp <123326315+zeyzeppp@users.noreply.github.com>
2023-03-25 17:24:42 +0300

    Add files via upload

---
+++ math.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main64()
+{
+    int sayi1, sayi2, sayi3;
+    sayi1 = 5;
+    sayi2 = 6;
+    sayi3 = 4;
+    float ort = (sayi1+sayi2+sayi3)/3;
+    printf("sayilarin ortalaması = %f", ort);
+
+}
