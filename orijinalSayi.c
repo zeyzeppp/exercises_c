b27e90b7dd44334b2666a0af4c5ab52ab9b6648a
zeyzeppp <123326315+zeyzeppp@users.noreply.github.com>
2023-03-27 18:13:32 +0300

    Add files via upload

---
+++ orijinalSayi.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main60()
+{
+      int sayi,sum1,sum2,sum,i;
+      int basamak[4];
+
+      printf("dort basamakli bir sayi giriniz: ");
+      scanf("%d", &sayi);
+
+      while(sayi > 10)
+      {
+            for(i = 0; i < 4; i++)
+            {
+                  basamak[i] = sayi%10;
+                  sayi = sayi/10;
+
+            }
+      }
+      sum1 = basamak[0] + basamak[1];
+      sum2 = basamak[2] + basamak[3];
+      sum = sum1 + sum2;
+
+      if(sayi == sum)
+      {
+            printf("orijinal sayi");
+      }
+      else
+      {
+            printf("orijinal sayi degil");
+      }
+
+
+
+}
