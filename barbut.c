7d2ad2532561c8c83937b99b85ef11194dc77ae4
zeyzeppp <123326315+zeyzeppp@users.noreply.github.com>
2023-04-04 23:28:41 +0300

    Add files via upload

---
+++ barbut.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//barbut oyunu
+
+int barbut(void);
+
+int main54()
+{
+      int toplam, oyuncununSayisi;
+
+      srand(time(NULL));
+
+      toplam = barbut();
+      printf("%d\n", toplam);
+
+      if(toplam == 11 || toplam == 7)
+      {
+            printf("kazandiniz!");
+      }
+      else if(toplam == 2 || toplam == 3 || toplam == 12)
+      {
+            printf("kaybettiniz!");
+      }
+      else
+      {
+            oyuncununSayisi = toplam;
+
+            toplam = barbut();
+
+            if(toplam == 7)
+            {
+                  printf("kaybettiniz!");
+            }
+
+            while(toplam != 7)
+            {
+                  toplam = barbut();
+                  printf("%d\n", toplam);
+
+                  if(toplam == oyuncununSayisi)
+                  {
+                        printf("kazandiniz!");
+                        break;
+                  }
+                  if(toplam == 7)
+                  {
+                        printf("kaybettiniz!");
+                  }
+            }
+
+      }
+
+      return 0;
+}
+
+int barbut(void)
+{
+      int zar1,zar2,zar_toplam;
+      zar1 = (rand()%6 + 1);
+      zar2 = (rand()%6 + 1);
+      zar_toplam = zar1 + zar2;
+      return zar_toplam;
+}
