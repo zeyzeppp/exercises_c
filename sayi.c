ec06ddfef7c0fbd381b1d816b4a158f47098ac85
zeyzeppp <123326315+zeyzeppp@users.noreply.github.com>
2023-04-04 12:15:46 +0300

    Add files via upload

---
+++ sayi.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int fact(int);
+int main()
+{
+      int sayi;
+      printf("bir sayi giriniz: ");
+      scanf("%d", &sayi);
+
+      if(sayi < 5)
+      {
+            sayi = fact(sayi);
+            printf("%d", sayi);
+      }
+      else if(sayi > 10)
+      {
+            double sayi1 = ((sayi/2) - 1);
+            printf("%f", sayi1);
+      }
+      else
+      {
+            int sayi2 = pow(sayi, 2);
+            printf("%d", sayi2);
+      }
+
+      return 0;
+}
+int fact(int num)
+{
+      if(num == 0 || num == 1)
+      {
+            return 1;
+      }
+
+      return fact(num-1)*num;
+}
