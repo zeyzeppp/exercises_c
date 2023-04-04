a1413dbb94cc7394f69bcb5a8a6218be938b6862
zeyzeppp <123326315+zeyzeppp@users.noreply.github.com>
2023-04-04 15:57:37 +0300

    Add files via upload

---
+++ karekok.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main55()
+{
+      int sayi;
+      float karekok = sqrt(sayi);
+
+      printf("bir sayi giriniz: ");
+      scanf("%d", &sayi);
+      printf("%d sayisinin karekoku = %f", sayi, karekok);
+
+      return 0;
+
+}
+
