64b58b14e2dd4028e5f28d2391994f29bc26e484
zeyzeppp <123326315+zeyzeppp@users.noreply.github.com>
2023-05-01 22:31:05 +0300

    Add files via upload

---
+++ hexadecimal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main29()
+{
+
+      int sayi;
+
+      printf("bir sayi giriniz: ");
+      scanf("%d", &sayi);
+
+      printf("sayi : %d\n", sayi);
+      printf("sayinin adresi : %x", &sayi);
+
+      return 0;
+}
