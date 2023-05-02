103d06ab018e3cf3f4a02591e2f765b245c5b213
Zeynep Alperen <123326315+zeyzeppp@users.noreply.github.com>
2023-05-02 21:48:27 +0300

    Add files via upload

---
+++ harf_sayisi.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main26()
+{
+
+      char kelime[100];
+      int sayac = 0;
+
+      int i;
+
+      printf("bir kelime yaziniz: ");
+      scanf("%s", kelime);
+
+      for(i = 0; i < 100; i++)
+      {
+
+            if(kelime[i] != '\0')
+            {
+                  sayac++;
+            }
+
+            else
+            {
+                  break;
+            }
+      }
+
+      printf("%d", sayac);
+
+
+
+}
